28be3f67ac668e78d05eea2df1640b5f5962f9b5
qingyun <maqingyun64@163.com>
2018-02-08 12:12:43 +0800

    change ViewController.swift

--- LearningGit/LearningGit/ViewController.swift
+++ LearningGit/LearningGit/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
         print("我进入了VC文件")
         
     }
