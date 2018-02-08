480ffaf69de1bcfc1e0fb7722fab67f7cee89c9b
qingyun <maqingyun64@163.com>
2018-02-08 14:55:07 +0800

    add LearningGit/LearningGit/WhatViewController.swift

---
+++ LearningGit/LearningGit/WhatViewController.swift
@@ -0,0 +1,35 @@
+//
+//  WhatViewController.swift
+//  LearningGit
+//
+//  Created by 马庆云 on 2018/2/8.
+//  Copyright © 2018年 马庆云. All rights reserved.
+//
+
+import UIKit
+
+class WhatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
