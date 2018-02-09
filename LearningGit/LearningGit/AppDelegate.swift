c5d2528847044cb21f78c2fa3c5c0dba7f6642b0
qingyun <maqingyun64@163.com>
2018-02-09 14:40:58 +0800

    test reuplod i just test

--- LearningGit/LearningGit/AppDelegate.swift
+++ LearningGit/LearningGit/AppDelegate.swift
@@ -13,6 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    //在此测试重交说明
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
